426a26a56f0af2aae82b170f393de88c5f786af8
JohnCase38 <john.case296@gmail.com>
2022-08-30 19:49:22 -0400

    [trying to push]

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,7 @@
-DROP DATABASE IF EXISTS locations_db;
-CREATE DATABASE locations_db;
-
-USE locations_db;
-
 DROP DATABASE IF EXISTS user_db;
 CREATE DATABASE user_db;
 
 USE user_db;
-
-CREATE TABLE locations (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    location_name VARCHAR(50) NOT NULL
-);
 
 CREATE TABLE reviews (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
