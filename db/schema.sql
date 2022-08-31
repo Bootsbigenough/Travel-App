b6e139e685f56c7cda5eec0b76b5794db8e8f225
JohnCase38 <john.case296@gmail.com>
2022-08-30 20:56:26 -0400

    [first push]

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,3 @@ CREATE DATABASE user_db;
 
 USE user_db;
 
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    location_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (location_id)
-    REFERENCES locations(id)
-    ON DELETE SET NULL
-);
-
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(18) NOT NULL,
-    FOREIGN KEY ()
-)
