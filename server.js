5bf4a2e71293322f9f3e6a2b0b7262e3309efe11
calihuddleston <calihuddleston@gmail.com>
2022-09-01 19:28:02 -0400

    'pushing front-end work...so far...'

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require ('express');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const app = express () ; 
+const app = express();
 
 const port = 3000;
 
-const handlebars = require ('express-handlebars');
+app.set("view engine", "handlebars");
 
-app.set ('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
 
-app.engine('handlebars', handlebars ( {
-    layoutsDir: __dirname + '/views/layouts',
-}));
+app.use(express.static("public"));
 
-app.use(express.static ('public'))
+app.get("/", (req, res) => {
+  res.render("main", { layout: "index" });
+});
 
-app.get('/', (req, res) => {
-    res.render('main', {layout : 'index'});
-    });
-
-app.listen(port, () => console.log (`App listening to port ${port} `)) ;
+app.listen(port, () => console.log(`App listening to port ${port}`));
